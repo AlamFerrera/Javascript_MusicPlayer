cf559c2ef4e616f82fa17d3e3ca482636f832baf
Alam Ferrera <alamferrera@gmail.com>
2021-11-11 12:31:47 -0400

    added progress bar click
    la cancion avanza al seleccionar en la barra de progreso

--- app.js
+++ app.js
@@ -62,6 +62,21 @@ function nextSong(){
     loadSong(songs[songIndex]);
     playSong();
 }
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
 //Event listeners
 
 playBtn.addEventListener('click', ()=> {
@@ -78,3 +93,6 @@ playBtn.addEventListener('click', ()=> {
 //Change song
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', setProgress);
+audio.addEventListener('ended', nextSong);
